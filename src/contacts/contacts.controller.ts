6ae16fc4a7b5adf1bd6b4511d7961a69faf4ed60
SinHuJung <tlsgn26@naver.com>
2021-11-29 17:55:27 +0900

    nestJS prisma & make api

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('contacts')
-export class ContactsController {}
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+  } from '@nestjs/common';
+  import { ContactsService } from './contacts.service';
+  import { Prisma } from '@prisma/client';
+  
+  @Controller('contacts')
+  export class ContactsController {
+    constructor(private readonly contactsService: ContactsService) {}
+    @Get()
+    getAll() {
+      return this.contactsService.getAll();
+    }
+  
+    @Get(':id')
+    get(@Param('id') id) {
+      return this.contactsService.get(Number(id));
+    }
+  
+    @Post()
+    create(@Body() contactCreateInput: Prisma.ContactCreateInput) {
+      return this.contactsService.create(contactCreateInput);
+    }
+  
+    @Delete(':id')
+    delete(@Param('id') id) {
+      return this.contactsService.delete(Number(id));
+    }
+  
+    @Put(':id')
+    update(
+      @Param('id') id,
+      @Body() contactUpdateInput: Prisma.ContactUpdateInput,
+    ) {
+      return this.contactsService.update(Number(id), contactUpdateInput);
+    }
+  }
--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class ContactsService {}
+export class ContactsService {
+  constructor(private readonly prismaService: PrismaService) {}
+  create(contactCreateInput: Prisma.ContactCreateInput) {
+    return this.prismaService.contact.create({
+      data: contactCreateInput,
+    });
+  }
+
+  getAll() {
+    return this.prismaService.contact.findMany();
+  }
+
+  delete(id: number) {
+    return this.prismaService.contact.delete({
+      where: { id },
+    });
+  }
+
+  get(id: number) {
+    return this.prismaService.contact.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: number, contactUpdateInput: Prisma.ContactUpdateInput) {
+    return this.prismaService.contact.update({
+      where: { id },
+      data: contactUpdateInput,
+    });
+  }
+}
