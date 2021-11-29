9464c8c072422a0872d4fdb37e68c0655602d116
SinHuJung <tlsgn26@naver.com>
2021-11-29 16:18:34 +0900

    main , module 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ContectsController } from './contects/contects.controller';
-import { ContactsService } from './contacts/contacts.service';
-import { ContactsController } from './contacts/contacts.controller';
 import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
   imports: [ContactsModule],
-  controllers: [AppController, ContectsController, ContactsController],
-  providers: [AppService, ContactsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
